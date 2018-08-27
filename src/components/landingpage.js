f6c10e1ea20e3e887a306b8f356ee1ea6d959771
Zubair Trabzada <zubair.trabzada@gmail.com>
2018-08-27 00:45:50 -0700

    updating routes

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class LandingPage extends Component {
     render() {
         return (
-            <div>
-                <h1>Landing Page</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                            src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+
+                        <div className="banner-text">   
+                            <h1>Fullstack Web Developer</h1>
+
+                            <hr/>
+
+                            <p>HTML | CSS | Javascript | React | NodeJS | Express | WordPress</p>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
