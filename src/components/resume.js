3047ac2c1084859a63b08d6e33d20fe05e2cc194
Zubair Trabzada <zubair.trabzada@gmail.com>
2018-11-17 13:57:40 -0800

    resume page complete

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 
+import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
+
 class Resume extends Component {
     render() {
         return (
@@ -31,7 +35,68 @@ class Resume extends Component {
                     </Cell>
                     
                     
-                    <Cell className="resume-right-col" col={8}>Right Side</Cell>    
+                    <Cell className="resume-right-col" col={8}>
+                        <h2>Education</h2>
+
+                        <Education 
+                            startYear={2002}
+                            endYear={2006}
+                            schoolName="My University"
+                            schoolDescription="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+                        />
+
+                        <Education 
+                            startYear={2002}
+                            endYear={2006}
+                            schoolName="My University"
+                            schoolDescription="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+                        />
+                        <hr style={{borderTop: '3px sold #e22947'}} />
+
+                        <h2>Experience</h2>
+
+                        <Experience
+                            startYear={2009}
+                            endYear={2012}
+                            jobName="First Job"
+                            jobDescription="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+                        />
+
+                        <Experience
+                            startYear={2009}
+                            endYear={2012}
+                            jobName="Second Job"
+                            jobDescription="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+                        />
+
+                        <Experience
+                            startYear={2009}
+                            endYear={2012}
+                            jobName="Third Job"
+                            jobDescription="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+                        />
+
+                        <hr style={{borderTop: '3px sold #e22947'}} />
+
+                        <h2>Skills</h2>
+                        <Skills 
+                            skill="Javascript"
+                            progress={90}
+                        />
+                        <Skills 
+                            skill="HTML/CSS"
+                            progress={60}
+                        />
+                        <Skills 
+                            skill="React"
+                            progress={80}
+                        />
+                        <Skills 
+                            skill="Solidity"
+                            progress={25}
+                        />
+
+                    </Cell>    
                 </Grid>    
             </div>
         )
