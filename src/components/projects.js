6fc1e61d45628e5ee4d9417632c15ddc9557fa30
Zubair Trabzada <zubair.trabzada@gmail.com>
2018-11-26 19:32:44 -0800

    updating projects page to add individual updated projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -10,94 +10,92 @@ class Projects extends Component {
     toggleCategorries() {
         if(this.state.activeTab === 0) {
             return (
-                <div className="projects-grid">
-                    {/* Project 1 */}
+                <div className="project-cards">
+                    <div className="projects-grid">
+                        {/* My Portfolio */}
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2017/08/20/14/37/eat-2661935__480.jpg")', backgroundSize: 'cover'}}>My Portfolio</CardTitle>
+                            <CardText>
+                                Youtube Api Project built with react and youtube api
+                            </CardText>
+                            <CardActions border>
+                                <Button colored><a href="https://github.com/ztrabzada1988/react-myportfolio" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
+                                <Button colored><a href="https://ztrabzada1988.github.io/react-myportfolio" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>LiveDemo</a></Button>
+                            </CardActions>
+                        </Card>
                     
+                        {/* YouTube API App */}
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://i.pinimg.com/originals/00/6f/fd/006ffd872df01f4e422965806dbc6a5d.png")', backgroundSize: 'cover'}}>YouTube API Project</CardTitle>
+                            <CardText>
+                                Youtube Api Project built with react and youtube api
+                            </CardText>
+                            <CardActions border>
+                                <Button colored><a href="https://github.com/ztrabzada1988/youtube-app" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
+                                <Button colored><a href="https://ztrabzada1988.github.io/youtube-app" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>LiveDemo</a></Button>
+                            </CardActions>
+                        </Card>
 
-                    {/* Project 2 */}
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', background: 'url(https://reactjs.org/logo-og.png) center / cover'}}>React Project #1</CardTitle>
-                        <CardText>
-                            Hello I am project one. I do cool things
-                        </CardText>
-                        <CardActions border>
-                            <Button colored>GitHub</Button>
-                            <Button colored>CodePen</Button>
-                            <Button colored>LiveDemo</Button>
-                        </CardActions>
-                        <CardMenu style={{color: 'rgb(88, 235, 149)'}}>
-                            <IconButton name="share"/>
-                        </CardMenu>
-                    </Card>
+                        {/* React Reminder App */}
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2017/01/18/15/07/temporal-distance-1990035__480.jpg")', backgroundSize: 'cover'}}>Task Reminder App</CardTitle>
+                            <CardText>
+                                Reminder app for tasks with deadlines and notices
+                            </CardText>
+                            <CardActions border>
+                                <Button colored><a href="https://github.com/ztrabzada1988/reminderApp" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
+                                <Button colored><a href="https://ztrabzada1988.github.io/reminderApp" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>LiveDemo</a></Button>
+                            </CardActions>
+                        </Card>
 
-                    {/* Project 3 */}
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', background: 'url(https://reactjs.org/logo-og.png) center / cover'}}>React Project #1</CardTitle>
-                        <CardText>
-                            Hello I am project one. I do cool things
-                        </CardText>
-                        <CardActions border>
-                            <Button colored>GitHub</Button>
-                            <Button colored>CodePen</Button>
-                            <Button colored>LiveDemo</Button>
-                        </CardActions>
-                        <CardMenu style={{color: 'rgb(88, 235, 149)'}}>
-                            <IconButton name="share"/>
-                        </CardMenu>
-                    </Card>
+                        {/* Photo Finder */}
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2016/10/21/13/01/pixabay-1757980__480.jpg")', backgroundSize: 'cover'}}>Image Finder App</CardTitle>
+                            <CardText>
+                                Reminder app for tasks with deadlines and notices
+                            </CardText>
+                            <CardActions border>
+                                <Button colored><a href="https://github.com/ztrabzada1988/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
+                                <Button colored><a href="https://ztrabzada1988.github.io/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>LiveDemo</a></Button>
+                            </CardActions>
+                        </Card>
+                    </div>
 
-                    {/* Project 4 */}
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', background: 'url(https://reactjs.org/logo-og.png) center / cover'}}>React Project #1</CardTitle>
-                        <CardText>
-                            Hello I am project one. I do cool things
-                        </CardText>
-                        <CardActions border>
-                            <Button colored>GitHub</Button>
-                            <Button colored>CodePen</Button>
-                            <Button colored>LiveDemo</Button>
-                        </CardActions>
-                        <CardMenu style={{color: 'rgb(88, 235, 149)'}}>
-                            <IconButton name="share"/>
-                        </CardMenu>
-                    </Card>
+                    <div className="projects-grid">
+                        {/* Photo Finder */}
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2016/10/21/13/01/pixabay-1757980__480.jpg")', backgroundSize: 'cover'}}>Image Finder App</CardTitle>
+                            <CardText>
+                                Reminder app for tasks with deadlines and notices
+                            </CardText>
+                            <CardActions border>
+                                <Button colored><a href="https://github.com/ztrabzada1988/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
+                                <Button colored><a href="https://ztrabzada1988.github.io/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>LiveDemo</a></Button>
+                            </CardActions>
+                        </Card>
+                    </div>
+
                 </div>
+
             )
         } else if(this.state.activeTab === 1) {
             return (
                 <div className="projects-grid">
-                    {/* YouTube API App */}
+                    
+                    {/* Photo Finder */}
                     <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://i.pinimg.com/originals/00/6f/fd/006ffd872df01f4e422965806dbc6a5d.png")', backgroundSize: 'cover'}}>YouTube API Project</CardTitle>
-                        <CardText>
-                            Youtube Api Project built with react and youtube api
-                        </CardText>
-                        <CardActions border>
-                            <Button colored><a href="https://github.com/ztrabzada1988/youtube-app" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
-                            <Button colored><a href="https://ztrabzada1988.github.io/youtube-app" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>LiveDemo</a></Button>
-                        </CardActions>
-                    </Card>
-
-                    {/* React Reminder App */}
-                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://i.pinimg.com/originals/00/6f/fd/006ffd872df01f4e422965806dbc6a5d.png")', backgroundSize: 'cover'}}>YouTube API Project</CardTitle>
+                        <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2016/10/21/13/01/pixabay-1757980__480.jpg")', backgroundSize: 'cover'}}>Image Finder App</CardTitle>
                         <CardText>
                             Reminder app for tasks with deadlines and notices
                         </CardText>
                         <CardActions border>
-                            <Button colored><a href="https://github.com/ztrabzada1988/reminderApp" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
-                            <Button colored><a href="https://ztrabzada1988.github.io/reminderApp" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>LiveDemo</a></Button>
+                            <Button colored><a href="https://github.com/ztrabzada1988/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
+                            <Button colored><a href="https://ztrabzada1988.github.io/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>LiveDemo</a></Button>
                         </CardActions>
                     </Card>
                 </div>
             );
         } else if(this.state.activeTab === 2) {
-            return (
-                <Card shadow={5} style={{minWidth: '45'}}>
-                    <h4>React JS Projects</h4>
-                </Card>
-            )
-        } else if(this.state.activeTab === 3) {
             return (
                 <Card shadow={5} style={{minWidth: '45'}}>
                     <h4>React JS Projects</h4>
@@ -110,7 +108,6 @@ class Projects extends Component {
         return (
             <div className="catagory-tabs">
                 <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-                    <Tab style={{color: '#2bd49e'}}>Javascript</Tab>
                     <Tab style={{color: '#2bd49e'}}>React</Tab>
                     <Tab style={{color: '#2bd49e'}}>Ethereum</Tab>
                     <Tab style={{color: '#2bd49e'}}>Others</Tab>
