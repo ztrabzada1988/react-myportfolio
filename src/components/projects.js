02eaa9457deb3de0b4c62ee81c0f2bdacca7fff4
Zubair Trabzada <ztrabzada@losaltosca.gov>
2018-11-27 17:24:57 -0800

    styling project cards for better visibility

--- src/components/projects.js
+++ src/components/projects.js
@@ -13,10 +13,11 @@ class Projects extends Component {
                 <div className="project-cards">
                     <div className="projects-grid">
                         {/* My Portfolio */}
-                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2017/08/20/14/37/eat-2661935__480.jpg")', backgroundSize: 'cover'}}>My Portfolio</CardTitle>
-                            <CardText>
-                                Youtube Api Project built with react and youtube api
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto', borderRadius: '10px'}}>
+                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '220px', backgroundImage: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSi-blSQwnpwJ9ODkRyfjs3QQYdekT2GtNs9uMwVnypzvK56MF9g")', backgroundSize: 'cover'}}></CardTitle>
+                            <CardText style={{ color: '#222222' }}>
+                                <h6 style={{ margin: '0px', fontWeight: 'bold', paddingBottom: '8px' }}>My Portfolio</h6>
+                                Youtube Api Project built with react and youtube api. Youtube Api Project built with react and youtube api
                             </CardText>
                             <CardActions border>
                                 <Button colored><a href="https://github.com/ztrabzada1988/react-myportfolio" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
@@ -25,10 +26,11 @@ class Projects extends Component {
                         </Card>
                     
                         {/* YouTube API App */}
-                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://i.pinimg.com/originals/00/6f/fd/006ffd872df01f4e422965806dbc6a5d.png")', backgroundSize: 'cover'}}>YouTube API Project</CardTitle>
-                            <CardText>
-                                Youtube Api Project built with react and youtube api
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto', borderRadius: '10px'}}>
+                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '220px', backgroundImage: 'url("https://i.pinimg.com/originals/00/6f/fd/006ffd872df01f4e422965806dbc6a5d.png")', backgroundSize: 'cover'}}></CardTitle>
+                            <CardText style={{ color: '#222222' }}>
+                                <h6 style={{ margin: '0px', fontWeight: 'bold', paddingBottom: '8px' }}>YouTube API Project</h6>
+                                Youtube Api Project built with react and youtube api. Youtube Api Project built with react and youtube api
                             </CardText>
                             <CardActions border>
                                 <Button colored><a href="https://github.com/ztrabzada1988/youtube-app" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
@@ -37,10 +39,11 @@ class Projects extends Component {
                         </Card>
 
                         {/* React Reminder App */}
-                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2017/01/18/15/07/temporal-distance-1990035__480.jpg")', backgroundSize: 'cover'}}>Task Reminder App</CardTitle>
-                            <CardText>
-                                Reminder app for tasks with deadlines and notices
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto', borderRadius: '10px'}}>
+                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '220px', backgroundImage: 'url("http://citycampus.gr/wp-content/uploads/2018/08/072518-to-do-list.jpg")', backgroundSize: 'cover'}}></CardTitle>
+                            <CardText style={{ color: '#222222' }}>
+                                <h6 style={{ margin: '0px', fontWeight: 'bold', paddingBottom: '8px' }}>Task Reminder App</h6>
+                                Youtube Api Project built with react and youtube api. Youtube Api Project built with react and youtube api
                             </CardText>
                             <CardActions border>
                                 <Button colored><a href="https://github.com/ztrabzada1988/reminderApp" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
@@ -49,10 +52,11 @@ class Projects extends Component {
                         </Card>
 
                         {/* Photo Finder */}
-                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2016/10/21/13/01/pixabay-1757980__480.jpg")', backgroundSize: 'cover'}}>Image Finder App</CardTitle>
-                            <CardText>
-                                Reminder app for tasks with deadlines and notices
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto', borderRadius: '10px'}}>
+                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '220px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2016/10/21/13/01/pixabay-1757980__480.jpg")', backgroundSize: 'cover'}}></CardTitle>
+                            <CardText style={{ color: '#222222' }}>
+                                <h6 style={{ margin: '0px', fontWeight: 'bold', paddingBottom: '8px' }}>Image Finder App</h6>
+                                Youtube Api Project built with react and youtube api. Youtube Api Project built with react and youtube api
                             </CardText>
                             <CardActions border>
                                 <Button colored><a href="https://github.com/ztrabzada1988/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
@@ -61,12 +65,13 @@ class Projects extends Component {
                         </Card>
                     </div>
 
-                    <div className="projects-grid">
+                    <div className="projects-grid" style= {{ marginTop: '50px' }}>
                         {/* Photo Finder */}
-                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2016/10/21/13/01/pixabay-1757980__480.jpg")', backgroundSize: 'cover'}}>Image Finder App</CardTitle>
-                            <CardText>
-                                Reminder app for tasks with deadlines and notices
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto', borderRadius: '10px'}}>
+                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '220px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2016/10/21/13/01/pixabay-1757980__480.jpg")', backgroundSize: 'cover'}}></CardTitle>
+                            <CardText style={{ color: '#222222' }}>
+                                <h6 style={{ margin: '0px', fontWeight: 'bold', paddingBottom: '8px' }}>Image Finder App</h6>
+                                Youtube Api Project built with react and youtube api. Youtube Api Project built with react and youtube api
                             </CardText>
                             <CardActions border>
                                 <Button colored><a href="https://github.com/ztrabzada1988/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
@@ -84,7 +89,7 @@ class Projects extends Component {
                     
                     {/* Photo Finder */}
                     <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                        <CardTitle style={{color: 'rgb(88, 235, 149)', height: '200px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2016/10/21/13/01/pixabay-1757980__480.jpg")', backgroundSize: 'cover'}}>Image Finder App</CardTitle>
+                        <CardTitle style={{color: 'rgb(88, 235, 149)', height: '220px', backgroundImage: 'url("https://cdn.pixabay.com/photo/2016/10/21/13/01/pixabay-1757980__480.jpg")', backgroundSize: 'cover'}}>Image Finder App</CardTitle>
                         <CardText>
                             Reminder app for tasks with deadlines and notices
                         </CardText>
