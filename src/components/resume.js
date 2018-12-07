0285a38c4f82b816bde4b04bd331c8ccc1553c43
Zubair Trabzada <zubair.trabzada@gmail.com>
2018-12-06 22:45:27 -0800

    updating resume page with more job descriptions

--- src/components/resume.js
+++ src/components/resume.js
@@ -62,22 +62,51 @@ class Resume extends Component {
                                     <li>Aid in marketing company’s products and services to potential customers.</li>
                                     <li>Provide user research and be the voice of customers to the company</li>
                                     <li>Explain complex blockchain and cryptocurrency topics to non-technical customers.</li>
+                                    <li>Constantly research blockchain and cryptocurrency regulations to provide the most</li>
+                                    <li>updated information to the founders for better product delivery.</li>
+                                    <li>Study cryptocurrency markets and provide user behavior report to recommend future business opportunities for the company.</li>
+                                    <li>Provide weekly reports on cryptocurrency market liquidity on different exchanges(mainly Coinbase, Binance, Bittrex, Poloniex).</li>
+                                    <li>Attend blockchain meetups and conferences in the San Francisco Bay Area to study overall blockchain community interest.</li>
+                                    <li>Observing community strength and involvement of new blockchain projects by participating in all online media platforms (reddit, twitter, slack, telegram, and etc).</li>
                                 </ul>}
                         />
 
                         <Experience
                             startYear={2009}
                             endYear={2012}
-                            jobName="Second Job"
-                            jobDescription="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+                            jobName="Cryptocurrency/Blockchain Researcher, Writer, Investor, Marketer (Freelance Work/Personal Projects)"
+                            jobDescription={
+                                <ul>
+                                    <li>Passionately researching blockchain projects and new cryptocurrencies to gain more knowledge and invest appropriately.</li>
+	                                <li>Attending meetups in the San Francisco Bay Area to meet founding members of new blockchain companies and developers.</li>
+                                    <li>Researching and writing (medium.com) articles on blockchain, cryptography, and cryptocurrencies.
+                                    <li>Doing one-on-one marketing for ICOs and other cryptocurrencies online (reddit.com, discord, telegram) and during evening meetups in the San Francisco Bay Area.</li>
+                                    <li>Thoroughly evaluating white papers and GitHub code of new blockchain projects before investing.</li>
+                                    <li>Investigating technical and marketing team members (LinkedIn profiles, meeting one-on- one during meetups if possible, looking at education background, and etc.) of new blockchain project before investing.</li>
+                                    </li>
+                                </ul>
+                            }
                         />
+
 
                         <Experience
                             startYear={2009}
                             endYear={2012}
-                            jobName="Third Job"
-                            jobDescription="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+                            jobName="Project Engineer at C&S Engineers Inc & Langan "
+                            jobDescription={
+                                <ul>
+                                    <li>Training new engineers on daily tasks.</li>
+                                    <li>Prioritizing capital improvement projects and managing contractors from start to finish.</li>
+                                    <li>Reviewing calculations and plans of contracted engineers.</li>
+                                    <li>Handling daily communications between public and staff for the department.</li>
+                                    <li>Incorporating new software to make routine tasks more efficient.</li>
+                                    <li>Overall management (reports, calculation, meetings, and other tasks) of company owned.</li>
+                                    <li>and privately contracted projects related to the engineering department.</li>
+
+                                </ul>
+                            }
                         />
+
 
                         <hr style={{borderTop: '3px sold #e22947'}} />
 
