1cbc674698014eba46e1ba5a3409be61917522c8
Zubair Trabzada <zubair.trabzada@gmail.com>
2018-12-16 14:28:08 -0800

    updating projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -143,18 +143,7 @@ class Projects extends Component {
             return (
                 <div className="project-cards">
                     <div className="projects-grid">
-                        {/* Ethereum Kickstarter */}
-                        <Card shadow={5} style={{minWidth: '450', borderRadius: '10px', marginLeft: '10px', marginRight: '10px' }}>
-                            <CardTitle style={{color: 'rgb(88, 235, 149)', height: '220px', backgroundImage: 'url("http://www.trbimg.com/img-58643812/turbine/ct-equity-crowdfunding-2016-predictions-bsi-20161229")', backgroundSize: 'cover'}}></CardTitle>
-                            <CardText style={{ color: '#222222' }}>
-                                <h6 style={{ margin: '0px', fontWeight: 'bold', paddingBottom: '8px' }}>Ethereum Crowd Fundraiser</h6>
-                                Youtube Api Project built with react and youtube api. Youtube Api Project built with react and youtube api
-                            </CardText>
-                            <CardActions border>
-                                <Button colored><a href="https://github.com/ztrabzada1988/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
-                                <Button colored><a href="https://ztrabzada1988.github.io/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>LiveDemo</a></Button>
-                            </CardActions>
-                        </Card>
+                        
 
                         {/* Bitypes.com */}
                         <Card shadow={5} style={{minWidth: '450', marginLeft: '10px', marginRight: '10px', borderRadius: '10px'}}>
@@ -172,7 +161,7 @@ class Projects extends Component {
 
                     <h5 style={{ textAlign: 'center' }}>Research Articles I have Written About Blockchain and Cryptocurrencies on medium.com</h5>
                     <div className="projects-grid" style= {{ marginTop: '30px'}}>
-                        {/* Ethereum Kickstarter */}
+                        {/* Crypto 101: A simple guide */}
                         <Card shadow={5} style={{minWidth: '450', margin: '10px', borderRadius: '10px'}}>
                             <CardTitle style={{color: 'rgb(88, 235, 149)', height: '220px', backgroundImage: 'url("https://cdn-images-1.medium.com/max/1600/1*qrR3ZryKcoqUgKhd9316Rg.jpeg")', backgroundSize: 'cover'}}></CardTitle>
                             <CardText style={{ color: '#222222' }}>
@@ -180,12 +169,11 @@ class Projects extends Component {
                                 Youtube Api Project built with react and youtube api. Youtube Api Project built with react and youtube api
                             </CardText>
                             <CardActions border>
-                                <Button colored><a href="https://github.com/ztrabzada1988/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
-                                <Button colored><a href="https://ztrabzada1988.github.io/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>LiveDemo</a></Button>
+                                <Button colored><a href="https://ztrabzada1988.github.io/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>Read Article</a></Button>
                             </CardActions>
                         </Card>
 
-                        {/* Ethereum Lottery */}
+                        {/* 5 Cryptocurrencies to watch in 2018 */}
                         <Card shadow={5} style={{minWidth: '450', margin: '10px', borderRadius: '10px'}}>
                             <CardTitle style={{color: 'rgb(88, 235, 149)', height: '220px', backgroundImage: 'url("https://cdn-images-1.medium.com/max/1600/1*UEKXA0Uy-ahEAdn0IcdR3Q.jpeg")', backgroundSize: 'cover'}}></CardTitle>
                             <CardText style={{ color: '#222222' }}>
@@ -193,12 +181,11 @@ class Projects extends Component {
                                 Youtube Api Project built with react and youtube api. Youtube Api Project built with react and youtube api
                             </CardText>
                             <CardActions border>
-                                <Button colored><a href="https://github.com/ztrabzada1988/ethereum-lottery-app-react/" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>GitHub</a></Button>
-                                <Button colored><a href="https://ztrabzada1988.github.io/ethereum-lottery-app-react" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>LiveDemo</a></Button>
+                                <Button colored><a href="https://ztrabzada1988.github.io/photo-search" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>Read Article</a></Button>
                             </CardActions>
                         </Card>
 
-                        {/* Bitypes.com */}
+                        {/* 5 Reasons why NEO is still undervalued */}
                         <Card shadow={5} style={{minWidth: '450', margin: '11px', borderRadius: '10px'}}>
                         <CardTitle style={{color: 'rgb(88, 235, 149)', height: '220px', backgroundImage: 'url("https://cdn-images-1.medium.com/max/1600/1*Qqs_urBSE9u2OmB5HgYX9g.jpeg")', backgroundSize: 'cover'}}></CardTitle>
                         <CardText style={{ color: '#222222' }}>
@@ -206,7 +193,7 @@ class Projects extends Component {
                             Youtube Api Project built with react and youtube api. Youtube Api Project built with react and youtube api
                         </CardText>
                         <CardActions border>
-                            <Button colored><a href="https://bitypes.com" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>Website</a></Button>
+                            <Button colored><a href="https://bitypes.com" target="blank" style={{ textDecoration: 'none', color: 'blue' }}>Read Article</a></Button>
                         </CardActions>
                         </Card>
                     </div>
