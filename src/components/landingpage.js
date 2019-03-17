84d7519aa81a813dc06de062bef6a4af3fe76af3
Zubair Trabzada <zubair.trabzada@gmail.com>
2019-03-17 12:46:41 -0700

    update landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,51 +1,70 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class LandingPage extends Component {
-    render() {
-        return (
-            <div style={{width: '100%', margin: 'auto'}}>
-                <Grid className="landing-grid">
-                    <Cell col={12}>
-                        <img 
-                            src={require('../img/code-cutout.png')}
-                            alt="avatar"
-                            className="avatar-img"
-                        />
+  render() {
+    return (
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src={require("../img/code-cutout.png")}
+              alt="avatar"
+              className="avatar-img"
+            />
 
-                        <div className="banner-text">   
-                            <h1>Web Developer & Blockchain Enthusiast</h1>
+            <div className="banner-text">
+              <h1>Sales/Solutions Engineer & Full Stack Developer</h1>
 
-                            <hr/>
+              <hr />
 
-                            <p>HTML | CSS | Javascript | React | Redux | NodeJS | Express | WordPress | Solidity | Material UI</p>
-                            <div className="social-links">
-                                {/* LinkedIn */}
-                                <a href="https://linkedin.com/in/zubair-trabzada"  rel="noopener noreferrer"  target="_blank">
-                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
-                                </a>
+              <p>
+                Sales | Customer Success | HTML | CSS | Javascript | React |
+                NodeJS | Express | WordPress | SQL
+              </p>
+              <div className="social-links">
+                {/* LinkedIn */}
+                <a
+                  href="https://linkedin.com/in/zubair-trabzada"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
 
-                                {/* GitHub */}
-                                <a href="https://github.com/ztrabzada1988?tab=overview&from=2017-12-01&to=2017-12-31"  rel="noopener noreferrer"  target="_blank">
-                                    <i className="fa fa-github-square" aria-hidden="true" />
-                                </a>
+                {/* GitHub */}
+                <a
+                  href="https://github.com/ztrabzada1988?tab=overview&from=2017-12-01&to=2017-12-31"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
 
-                                {/* Medium */}
-                                <a href="https://medium.com/@cryptonia"  rel="noopener noreferrer"  target="_blank">
-                                    <i className="fa fa-medium" aria-hidden="true" />
-                                </a>
+                {/* Medium */}
+                <a
+                  href="https://medium.com/@cryptonia"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-medium" aria-hidden="true" />
+                </a>
 
-                                {/* Medium */}
-                                <a href="mailto:zubair.trabzada@gmail.com"  rel="noopener noreferrer"  target="_blank">
-                                    <i className="fa fa-envelope" aria-hidden="true" />
-                                </a>
-                            </div>
-                        </div>
-                    </Cell>
-                </Grid>
+                {/* Medium */}
+                <a
+                  href="mailto:zubair.trabzada@gmail.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-envelope" aria-hidden="true" />
+                </a>
+              </div>
             </div>
-        )
-    }
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
